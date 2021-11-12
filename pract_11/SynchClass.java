60bc4ce9b2921de7602393de41c44053214de0ca
nothingIIersonal <73304319+nothingIIersonal@users.noreply.github.com>
2021-11-12 15:28:06 +0300

    Add files via upload

--- pract_11/SynchClass.java
+++ pract_11/SynchClass.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class SynchClass {
 
     private static class Horse extends Thread  {
-        public static int position = 0;            
-        
+
+        public static int position = 0;  
+        private String name;
+        private Semaphore sem;
+
+        Horse (Semaphore sem, String name) {
+            this.sem = sem;
+            this.name = name;
+        }
+
         @Override
         public void run() {
-            synchronized (Horse.class) {
-                System.out.println(position++ + " \t is \t - \t " + this.getName());
+            try {
+                this.sem.acquire();
+                System.out.println(++position + " \t is \t - \t " + this.name);
+                this.sem.release();
+            }
+            catch (InterruptedException ex) {
+                System.out.println("Error\n");
             }
         }
     }
@@ -21,11 +35,12 @@ public class SynchClass {
         System.out.printf("Enter horse count: ");
         count = scan.nextInt(); 
 
+        Semaphore sem = new Semaphore(1, true);
+
         Horse[] horses = new Horse[count];
         
         for (int i = 0; i < count; i++) {
-            horses[i] = new Horse();
-            horses[i].setName("Horse №" + i);
+            horses[i] = new Horse(sem, "Horse №" + (i+1));
         }
 
         for (int i = 0; i < count; i++) {
